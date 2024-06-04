f37f03b084aa3605858b612e6edefe76849e6944
proyectoCAC2024 <danielagrisel433@gmail.com>
2024-06-04 15:46:28 -0300

    first commit

---
+++ new shop/src/components/ProductList.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {Data} from '../data';
+
+export const ProductList = ({allProducts, setAllProducts,countProductd,
+	setCountProducts,total,setTotal}) =>{
+	
+	const onAddProduct = product => {
+
+	if(allProducts.find(item => item.id === product.id)){
+		const products = allProducts.map(item => 
+			item.id === product.id ? {...item, quantity: item.quantity + 1} : item);
+		
+		setTotal(total + product.price * product.quantity) ;
+		setCountProducts(setCountProducts + product.quantity);
+		return setAllProducts([...products]);
+	}
+	    setTotal(total + product.price * product.quantity) ;
+	    setCountProducts(setCountProducts + product.quantity);
+		setAllProducts([...allProducts, product]);
+		
+	};
+
+	return (
+		<div className="container-items">
+			{Data.map((product) => (
+				<div className="item" key={product.id}> 
+					<figure>
+						<img
+							src={product.img}
+							alt={product.nameProduct}
+						/>
+					</figure>
+					<div className="info-product">
+						<h2>{product.nameProduct}</h2>
+						<p className="price">${product.price}</p>
+						<button onClick={() => onAddProduct(product)}>Añadir al carrito</button>
+					</div>
+				</div>
+			))}
+		</div>
+	);
+};
+
